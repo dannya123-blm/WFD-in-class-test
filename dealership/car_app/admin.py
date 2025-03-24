331f99247117c5631740d25ea46e339510e3f23b
dannya123-blm <B00150058@mytudublin.ie>
2025-03-24 15:29:01 +0000

    Superuser:
    username = danny
    email = dannyaigbe4@gmail.com
    password = Seandan123
    
    <<<<<<<This is just for debugging purposes, in amin to see all models created>>>>>>>

--- dealership/car_app/admin.py
+++ dealership/car_app/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Customer, Car, Salesperson, SalesInvoice, Mechanic, Service, ServiceTicket, ServiceMechanic, Part, PartsUsed
 
 # Register your models here.
+admin.site.register(Customer)
+admin.site.register(Car)
+admin.site.register(Salesperson)
+admin.site.register(SalesInvoice)
+admin.site.register(Mechanic)
+admin.site.register(Service)
+admin.site.register(ServiceTicket)
+admin.site.register(ServiceMechanic)
+admin.site.register(Part)
+admin.site.register(PartsUsed)
